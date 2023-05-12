255da70b66d4877e1c8aeff3175cb8a81c969eab
almeidafe <133253569+almeidafe@users.noreply.github.com>
2023-05-12 11:26:26 +0100

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/almeidafe/network/azurerm"
   version = "3.5.0"
-  resource_group_name = azurerm_network_interface
+  resource_group_name = "azurerm_network_interface"
   # insert required variables here
 }
