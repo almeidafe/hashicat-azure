107befad899df965c431d5154307e36281351d4c
almeidafe <133253569+almeidafe@users.noreply.github.com>
2023-05-12 11:44:52 +0100

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,6 +1,7 @@
 module "network" {
   source  = "app.terraform.io/almeidafe/network/azurerm"
   version = "3.5.0"
+  resource_group_name = "rgroup"
  
 resource "azurerm_resource_group" "example" {
   name     = "my-resources"
