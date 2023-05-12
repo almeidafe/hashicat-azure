e2e8e3a00c4802e257373edfa8af10d37ed6ec06
almeidafe <133253569+almeidafe@users.noreply.github.com>
2023-05-12 11:49:12 +0100

    Update network.tf

--- network.tf
+++ network.tf
@@ -8,8 +8,8 @@ resource "azurerm_resource_group" "example" {
 module "network" {
   source  = "app.terraform.io/almeidafe/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "rgroup"
-  source              = "Azure/network/azurerm"
+  #resource_group_name = "rgroup"
+  #source              = "Azure/network/azurerm"
   resource_group_name = azurerm_resource_group.example.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
